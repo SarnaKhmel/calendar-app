d3f3da3493cfcfbe2234423e696ade2995f8c965
sarnakhmel <olsarnat@gmail.com>
2024-10-20 17:32:01 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Calendar from './components/Calendar/ui/Calendar';
+import Layout from './components/Layout/Layout';
+import NotFound from './components/NotFound/NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () => {
+    const [activePage, setActivePage] = useState<string>('Calendar');
+
+    return (
+        <Layout activePage={activePage} setActivePage={setActivePage}>
+            {activePage === 'Calendar' ? (
+                <div className="calendar-page">
+                    <Calendar />
+                </div>
+            ) : (
+                <NotFound />
+            )}
+        </Layout>
+    );
 }
 
 export default App;
